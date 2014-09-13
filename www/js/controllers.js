f314ad2df678d54a34dc67ffa81020ef6fcbe6f3
Vishnu R Menon <vishthemenon@Vishnus-MacBook-Pro.local>
2014-09-13 16:56:27 +0800

    added swiping for campaigns

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,7 @@
 app.controller('CampaignsCtrl', function($scope, $firebase){
   var ref = new Firebase("https://wolfberry.firebaseio.com/campaigns");
   var sync = $firebase(ref);
+  $scope.cardCounter = 1;
   var campaignsArray = sync.$asArray();
   $scope.campaigns = campaignsArray;
   // $scope.campaigns = [
@@ -8,4 +9,17 @@ app.controller('CampaignsCtrl', function($scope, $firebase){
   //   {title: "Campaign 2"},
   //   {title: "Campaign 3"}
   // ]
+  $scope.cardLeft = function(){
+  	$scope.cardCounter = $scope.cardCounter+1;
+  	var i = ($scope.cardCounter)*-320;
+  	i = String(i) + "px";
+  	document.getElementsByClassName("campaignItem")[0].style.marginLeft = i;
+  };
+  $scope.cardRight = function(){
+  	$scope.cardCounter = $scope.cardCounter-1;
+  	var i = ($scope.cardCounter)*-320;
+  	i = String(i) + "px";
+  	document.getElementsByClassName("campaignItem")[0].style.marginLeft = i;
+  };
+
 })
